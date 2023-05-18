7093207da586b62f71a6c9d881e24cc69863e1b4
Pro <twisted.fall@gmail.com>
2023-05-18 20:08:42 +0200

    Update release script

--- tools/pre-release.sh
+++ tools/pre-release.sh
@@ -7,9 +7,6 @@ README="$CRATE_ROOT/README.md"
 
 if [[ "$DRY_RUN" == "false" ]]; then
 	cargo readme --template="$README_TPL" --output="$README"
-	if [[ "$CRATE_NAME" == "solaredge" ]]; then
-		cp -v "$README" "$WORKSPACE_ROOT/"
-	fi
 else
 	echo "Dry run, would generate $README from $README_TPL"
 fi
